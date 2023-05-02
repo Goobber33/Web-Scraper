2ffaf5f6e1c89c4e8c978525fdf41e9787dd040f
Kyle <kcparks1234@gmail.com>
2023-05-02 14:46:20 -0700

    Finished adding correct changes, the queue wasn't running correctly.

--- scraper.js
+++ scraper.js
@@ -33,62 +33,64 @@ const parseData = (html) => {
 };
 
 const saveData = (data, url, callback) => {
-    try {
-      const db = client.db(process.env.MONGODB_DATABASE_NAME);
-      const collection = db.collection(process.env.MONGODB_COLLECTION_NAME);
-  
-      collection.insertOne({ url, ...data }, (error, result) => {
-        if (error) {
-          console.error(`Error saving data to MongoDB Atlas for URL: ${url}: ${error}`);
-          return callback(error);
-        }
-  
-        console.log(`Data saved to MongoDB Atlas for URL: ${url}`);
-        callback(null);
-      });
-    } catch (error) {
-      console.error(`Error saving data to MongoDB Atlas for URL: ${url}: ${error}`);
-      callback(error);
-    }
-  };
-  
-  const processUrl = async (url, callback) => {
-    console.log(`Fetching ${url}`);
-    const html = await fetchData(url);
-  
-    if (html) {
-      console.log(`Parsing data from ${url}`);
-      const data = parseData(html);
-      saveData(data, url, callback);
-    } else {
-      callback();
-    }
-  };   
+  try {
+    const db = client.db(process.env.MONGODB_DATABASE_NAME);
+    const collection = db.collection(process.env.MONGODB_COLLECTION_NAME);
+
+    collection.insertOne({ url, ...data }, (error, result) => {
+      if (error) {
+        console.error(`Error saving data to MongoDB Atlas for URL: ${url}: ${error}`);
+        return callback(error);
+      }
+
+      console.log(`Data saved to MongoDB Atlas for URL: ${url}`);
+      callback(null);
+    });
+  } catch (error) {
+    console.error(`Error saving data to MongoDB Atlas for URL: ${url}: ${error}`);
+    callback(error);
+  }
+};
+
+const processUrl = async (url, callback) => {
+  console.log(`Fetching ${url}`);
+  const html = await fetchData(url);
+
+  if (html) {
+    console.log(`Parsing data from ${url}`);
+    const data = parseData(html);
+    saveData(data, url, callback);
+  } else {
+    callback();
+  }
+};
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const main = async () => {
-  try {
-    await client.connect();
-    console.log('Connected to MongoDB Atlas');
-
-    const queue = async.queue(processUrl, 2); // Rate limit: 2 concurrent requests
-
-    queue.push(urls, (error) => {
-      if (error) {
-        console.error(`Error processing URL: ${error}`);
-      } else {
-        console.log('URL processed successfully.');
-      }
-    });
-
-    queue.drain(() => {
-      console.log('All URLs have been processed.');
-      client.close();
-    });
-  } catch (error) {
-    console.error('Error connecting to MongoDB Atlas:', error);
-  }
-};
-
-main();
+    try {
+      await client.connect();
+      console.log('Connected to MongoDB Atlas');
+  
+      const queue = async.queue(processUrl, 2); // Rate limit: 2 concurrent requests
+  
+      queue.push(urls, (error) => {
+        if (error) {
+          console.error(`Error processing URL: ${error}`);
+        } else {
+          console.log('URL processed successfully.');
+        }
+      });
+  
+      queue.drain = async () => {
+        if (queue.idle()) {
+          console.log('All URLs have been processed.');
+          await client.close();
+        }
+      };
+    } catch (error) {
+      console.error('Error connecting to MongoDB Atlas:', error);
+    }
+  };
+  
+  main();
