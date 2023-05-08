ab3595de65c86b248bdc6def58465e256c14a6e3
Kyle <kcparks1234@gmail.com>
2023-05-08 16:19:22 -0700

    Reverted changes

--- scraper.js
+++ scraper.js
@@ -1,4 +1,10 @@
 const axios = require('axios');
+const async = require('async');
+const { MongoClient } = require('mongodb');
+
+require('dotenv').config();
+
+const uri = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.jslrvg2.mongodb.net/${process.env.MONGODB_DATABASE_NAME}?retryWrites=true&w=majority`;
 
 const getRandomInt = (max) => {
   return Math.floor(Math.random() * Math.floor(max)) + 1;
@@ -34,20 +40,7 @@ const parseData = (data, url) => {
   return parsedData;
 };
 
-const processUrl = async (client, url, callback) => {
-  console.log(`Fetching ${url}`);
-  const data = await fetchData(url);
-
-  if (data) {
-    console.log(`Parsing data from ${url}`);
-    const parsedData = parseData(data, url);
-    saveData(client, parsedData, url, callback);
-  } else {
-    callback();
-  }
-};
-
-const saveData = (client, data, url, callback) => {
+const saveData = (data, url, callback) => {
   try {
     const db = client.db(process.env.MONGODB_DATABASE_NAME);
     const collection = db.collection(process.env.MONGODB_COLLECTION_NAME);
@@ -67,4 +60,45 @@ const saveData = (client, data, url, callback) => {
   }
 };
 
-module.exports = { processUrl, urls };
+const processUrl = async (url, callback) => {
+  console.log(`Fetching ${url}`);
+  const data = await fetchData(url);
+
+  if (data) {
+    console.log(`Parsing data from ${url}`);
+    const parsedData = parseData(data, url); 
+    saveData(parsedData, url, callback);
+  } else {
+    callback();
+  }
+};
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const main = async () => {
+  try {
+    await client.connect();
+    console.log('Connected to MongoDB Atlas');
+
+    const queue = async.queue(processUrl, 2); // Rate limit: 2 concurrent requests
+
+    queue.push(urls[0], (error) => {
+      if (error) {
+        console.error(`Error processing URL: ${error}`);
+      } else {
+        console.log('URL processed successfully.');
+      }
+    });
+
+    queue.drain = async () => {
+      if (queue.idle()) {
+        console.log('All URLs have been processed.');
+        await client.close();
+      }
+    };
+  } catch (error) {
+    console.error('Error connecting to MongoDB Atlas:', error);
+  }
+};
+
+main();
--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const { MongoClient } = require('mongodb');
-const { processUrl, urls } = require('./scraper');
 
 require('dotenv').config();
 
-const app = express();
-app.use(cors());
-
 const uri = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.jslrvg2.mongodb.net/${process.env.MONGODB_DATABASE_NAME}?retryWrites=true&w=majority`;
 
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Connect to MongoDB
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const connectToMongoDB = async () => {
@@ -21,19 +22,31 @@ const connectToMongoDB = async () => {
   }
 };
 
+connectToMongoDB();
+
+app.use(cors());
+app.use(express.json());
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
+
 app.get('/api/data', async (req, res) => {
   try {
     const db = client.db(process.env.MONGODB_DATABASE_NAME);
     const collection = db.collection(process.env.MONGODB_COLLECTION_NAME);
 
-    const data = await collection.find({}).toArray();
-    res.status(200).json(data);
+    const data = await collection.find().toArray();
+    res.json(data);
   } catch (error) {
-    console.error('Error fetching data:', error);
-    res.status(500).json({ message: 'Error fetching data', error });
+    console.error('Error fetching data from MongoDB Atlas:', error);
+    res.status(500).json({ message: 'Error fetching data' });
   }
 });
 
+// Handle any requests that don't match the ones above
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'));
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
